905b278d58473df8ea2e05a8210bb256bac90633
cybergears <ssheikh111@gmail.com>
2022-03-29 23:10:09 +0530

    Added airdrop  wallet save functionality
    Now all the wallet addresses will be saved in the database,

---
+++ inc/wallet.php
@@ -0,0 +1,55 @@
+<?php 
+    //this will save the wallet address
+
+    ini_set('display_errors', '1'); //this hides error if the value = 0,  1 will display errors 
+
+    $status=0;
+    if(isset($_GET['address'])){
+        $wallet_address = filter_var($_GET['address'],FILTER_SANITIZE_STRING); //sanitize the string
+        if(empty($wallet_address)){
+            $status=0;
+        }else{
+            
+            //DB Link
+            
+            $username = 'root'; //db username
+            $password = ''; //db password
+            $host = 'localhost'; // db host
+            $db = 'mega_air_drop'; //db name
+            
+            // create connection
+
+            $con = mysqli_connect($host,$username,$password,$db);
+
+            if($con){
+
+                //check address if already exists
+                $check_address = mysqli_query($con, "SELECT * FROM airdrop_wallet WHERE wallet_address=".$wallet_address."");
+
+                if(mysqli_num_rows($check_address)>=1){
+                    $status = 2;
+                }else{
+
+                    //save address on database if not exists
+
+                    $address_query = "INSERT INTO airdrop_wallet(wallet_address) VALUES('$wallet_address')";
+                    $save_address = mysqli_query($con,$address_query);
+                    if($save_address){
+                        $status = 1;
+                    }else{
+                        $status = 0;
+                    }
+                }
+                
+            }else{
+                $status=0;
+            }
+            
+            $return_array = array();
+	 	    $return_array[] = array("status" => $status);
+	 	    echo  json_encode($return_array);
+        }
+
+    }
+
+?>
